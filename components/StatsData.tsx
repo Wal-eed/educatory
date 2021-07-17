1729a33301a3329606cab0842d76551bbc625fc6
Arthur Wang <ArthurWang2000@Hotmail.com>
2021-07-18 09:25:35 +1000

    Linked up stats with statsHeader

---
+++ components/StatsData.tsx
@@ -0,0 +1,32 @@
+const data = {
+    // math:
+    science: [
+        { date: '12 Jul', number: 2 },
+        { date: '13 Jul', number: 2 },
+        { date: '14 Jul', number: 5 },
+        { date: '15 Jul', number: 8 },
+        { date: '16 Jul', number: 7 },
+        { date: '17 Jul', number: 8 },
+        { date: '18 Jul', number: 7 },
+    ],
+    numeracy: [
+        { date: '12 Jul', number: 2 },
+        { date: '13 Jul', number: 3 },
+        { date: '14 Jul', number: 4 },
+        { date: '15 Jul', number: 2 },
+        { date: '16 Jul', number: 6 },
+        { date: '17 Jul', number: 7 },
+        { date: '18 Jul', number: 8 },
+    ],
+    logic: [
+        { date: '12 Jul', number: 1 },
+        { date: '13 Jul', number: 2 },
+        { date: '14 Jul', number: 3 },
+        { date: '15 Jul', number: 4 },
+        { date: '16 Jul', number: 5 },
+        { date: '17 Jul', number: 6 },
+        { date: '18 Jul', number: 6 },
+    ],
+};
+
+export default data;
