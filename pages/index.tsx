c9c71fe6989fdc970f9db0d6fc89171f29111009
Max Phillips <max@maxphillipsdev.com>
2021-07-17 16:26:48 +1000

    Add chakraUI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
+import { Flex, useToast, Button } from "@chakra-ui/react";
+
 const Home = () => {
+  const toast = useToast();
   return (
-    <div>
-      <p>home page</p>
-    </div>
+    <Flex
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      marginTop="50%"
+    >
+      <Button
+        onClick={() =>
+          toast({
+            title: "Yay it works!",
+            status: "success",
+            duration: 9000,
+            isClosable: true,
+          })
+        }
+      >
+        Does it work?
+      </Button>
+    </Flex>
   );
 };
 export default Home;
