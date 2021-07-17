6cc624092783df381446e1ef3444c15d5bd6f700
Max Phillips <max@maxphillipsdev.com>
2021-07-17 20:08:05 +1000

    Split column

--- pages/viz.tsx
+++ pages/viz.tsx
@@ -1,44 +1,24 @@
-import { Container } from "@chakra-ui/react";
-import { OrbitControls } from "@react-three/drei";
+import { Box, Container, Progress, SimpleGrid } from "@chakra-ui/react";
+import { Environment, OrbitControls, useProgress } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import React, { useRef, useState } from "react";
+import React, { Suspense, useRef, useState } from "react";
 import { Mesh } from "three";
-
-const Box = (props) => {
-  const mesh = useRef<Mesh>();
-
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
-
-  useFrame(() => {
-    if (mesh.current && !active)
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.02;
-  });
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHovered(true)}
-      onPointerOut={(e) => setHovered(false)}
-    >
-      <boxBufferGeometry args={[1, 1, 1]} />
-      <meshPhongMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-};
+import Model from "../components/Tree";
 
 const Viz: React.FC = () => {
+  const { progress } = useProgress();
+
   return (
-    <Container backgroundColor={"blue"} width={"300px"} height={"300px"}>
-      <Canvas>
+    <SimpleGrid columns={2} spacing={0} height="100vh">
+      <Canvas camera={{ position: [-25, 10, -25], fov: 90 }}>
         <ambientLight intensity={0.5} />
         <OrbitControls />
-        <Box position={[0, 0, 0]} />
+        <Suspense fallback={null}>
+          <Model />
+        </Suspense>
       </Canvas>
-    </Container>
+      <Box bg="tomato" />
+    </SimpleGrid>
   );
 };
 
