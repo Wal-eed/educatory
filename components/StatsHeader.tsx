ffc67fe0351e6650ff2a04114570aedac1be41b9
Arthur Wang <ArthurWang2000@Hotmail.com>
2021-07-17 22:35:37 +1000

    Styled stats header

--- components/StatsHeader.tsx
+++ components/StatsHeader.tsx
@@ -7,6 +7,7 @@ import { Heading } from '@chakra-ui/react';
 import Link from 'next/link';
 import styles from './StatsHeader.module.scss';
 import Image from 'next/image';
+import { ChevronRightIcon } from '@chakra-ui/icons';
 interface FeatureProps {
 	title: string;
 	text: string;
@@ -23,60 +24,55 @@ const Feature = ({ title, text, icon }: FeatureProps) => {
 
 export default function SimpleThreeColumns() {
 	return (
-		<Link href="stats" passHref>
-			<Flex justifyContent="center" style={{ margin: '2rem 0' }}>
-				<Box
-					p={4}
-					className={styles.outer_box}
-					style={{ position: 'relative', borderRadius: '10px' }}
-				>
-					<Heading>Hey Alex 👋</Heading>
-					<Text>Here's what we'll be doing today</Text>
-					<SimpleGrid columns={{ base: 2, md: 4 }} spacing={10}>
-						<Feature
-							icon={<Icon as={FcAssistant} w={10} h={10} />}
-							title={'Numeracy'}
-							text={
-								'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-							}
-						/>
-						<Feature
-							icon={<Icon as={FcDonate} w={10} h={10} />}
-							title={'Science'}
-							text={
-								'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-							}
-						/>
-						<Feature
-							icon={<Icon as={FcInTransit} w={10} h={10} />}
-							title={'Geography'}
-							text={
-								'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-							}
-						/>
-
-						{/* <Image src="/Study_art.png" alt="study image" width="128" height="112"/> */}
-					</SimpleGrid>
-					{/* <Link href="stats" passHref>
-					<Button colorScheme="blue" float="right">
-						Find out more
-					</Button>
-				</Link> */}
-					{/* <div style={{ position: 'absolute', right: 0 }}>
-					<Image src="/Study_art.png" alt="study image" width={200} height={200} />
-				</div> */}
-					<img
-						src="/Study_art.png"
-						alt="study image"
-						style={{
-							position: 'absolute',
-							top: -68,
-							right: -98,
-							height: '150%',
-						}}
+		<Flex justifyContent="center" style={{ margin: '2rem 0' }}>
+			<Box
+				p={4}
+				className={styles.outer_box}
+				style={{ position: 'relative', borderRadius: '10px' }}
+			>
+				<Heading>Hey Alex 👋</Heading>
+				<Text>Here's what we'll be doing today</Text>
+				<SimpleGrid columns={{ base: 2, md: 4 }} spacing={10}>
+					<Feature
+						icon={<Icon as={FcAssistant} w={10} h={10} />}
+						title={'Numeracy'}
+						text={
+							'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+						}
 					/>
-				</Box>
-			</Flex>
-		</Link>
+					<Feature
+						icon={<Icon as={FcDonate} w={10} h={10} />}
+						title={'Science'}
+						text={
+							'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+						}
+					/>
+					<Feature
+						icon={<Icon as={FcInTransit} w={10} h={10} />}
+						title={'Geography'}
+						text={
+							'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
+						}
+					/>
+				</SimpleGrid>
+				<Link href="stats" passHref>
+					<Flex>
+						<ChevronRightIcon w={6} h={6} />
+						<Text>See More</Text>
+					</Flex>
+				</Link>
+
+				<img
+					src="/Study_art.png"
+					alt="study image"
+					style={{
+						position: 'absolute',
+						top: -68,
+						right: -98,
+						height: '150%',
+					}}
+				/>
+			</Box>
+		</Flex>
 	);
 }
