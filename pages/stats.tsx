2b551fb19c7830936f142aa38c7c8114b4623677
Arthur Wang <ArthurWang2000@Hotmail.com>
2021-07-17 19:44:24 +1000

    Adding line chart

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -1,32 +1,40 @@
 import MultiDonut from 'components/MultiDonut';
 import React from 'react';
-import { Heading, Flex, SimpleGrid } from '@chakra-ui/react';
+import { Heading, Flex, SimpleGrid, Container } from '@chakra-ui/react';
 import HeatMap from 'components/HeatMap';
 import Radar from 'components/Radar';
-import StatsSideBar from 'components/StatsSideBar';
+// import StatsSideBar from 'components/StatsSideBar';
+import LineChart from 'components/LineChart';
 
 const Stats = () => {
 	const data = [
-		{ category: 'January', number: 40 },
-		{ category: 'January', number: 69 },
-		{ category: 'January', number: 20 },
-		{ category: 'January', number: 100 },
-		{ category: 'January', number: 25 },
-		{ category: 'January', number: 80 },
+		{ category: 'Math', number: 40 },
+		{ category: 'Math', number: 69 },
+		{ category: 'Math', number: 20 },
+		{ category: 'Math', number: 100 },
+		{ category: 'Math', number: 100 },
+		{ category: 'Math', number: 25 },
+		{ category: 'Math', number: 80 },
 	];
 	return (
 		<>
-			<Flex justifyContent="center">
-				<Heading>Progress</Heading>
-			</Flex>
-			<SimpleGrid columns={2} spacing={10}>
-				<MultiDonut data={data} />
-				<Radar data={data} />
-			</SimpleGrid>
-			<Flex justifyContent="center">
-				<Heading>Daily commitment</Heading>
-			</Flex>
-			<HeatMap />
+			<Container maxW="container.xl">
+				<Flex justifyContent="center">
+					<Heading>Progress</Heading>
+				</Flex>
+				<SimpleGrid columns={2} spacing={10}>
+					<MultiDonut data={data} />
+					<Radar data={data} />
+				</SimpleGrid>
+				<Flex justifyContent="center">
+					<Heading>Progress over time</Heading>
+				</Flex>
+				<LineChart />
+				<Flex justifyContent="center">
+					<Heading>Daily commitment</Heading>
+				</Flex>
+				<HeatMap />
+			</Container>
 		</>
 	);
 };
