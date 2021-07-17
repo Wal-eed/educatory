ef67ec62a961e6b025656a9ea19633fafe272966
Tymotex <timzhang3@gmail.com>
2021-07-18 09:23:50 +1000

    Linked pages

--- components/LessonSection.jsx
+++ components/LessonSection.jsx
@@ -75,7 +75,7 @@ const lessonCards = {
 			title: 'Riddles',
 			image:
 				'https://images.unsplash.com/photo-1615368946542-390a84479ec7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1867&q=80',
-			lessonType: 'audio',
+			lessonType: 'text',
 		},
 	],
 };
