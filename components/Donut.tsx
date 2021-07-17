4c538821bc5ca46eff05492ed641829a411f6dfb
Arthur Wang <ArthurWang2000@Hotmail.com>
2021-07-17 17:56:37 +1000

    Added donuts (radialbar) to stats header

---
+++ components/Donut.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+// import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const Donut = ({ label = "no label", percentage = 50, height = 200, children = null}) => {
+  var state = {
+    series: [percentage],
+    options: {
+      chart: {
+        height: height,
+        type: "radialBar",
+      },
+      plotOptions: {
+        radialBar: {
+          hollow: {
+            size: "70%",
+          },
+        },
+      },
+      labels: [label],
+    },
+  };
+
+  return (
+    <div id="chart">
+      <Chart
+        options={state.options}
+        series={state.series}
+        type="radialBar"
+        height={height}
+      />
+    </div>
+  );
+};
+export default Donut;
